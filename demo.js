fe105aec24166c216f4ca61990be9050b337aba1
shivaQA710 <shivaqa710@gmail.com>
2024-04-02 17:25:32 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var a = 10;
+var b = 20;
+var arr = [6, 1, 2, 3, 4, 5];
+arr.sort();
+console.log(Math.max.apply(Math, arr));
---
+++ demo.ts
@@ -0,0 +1,16 @@
+export{}
+
+
+let a=10;
+let b=20;
+
+
+const arr = [6,1, 2, 3, 4, 5];
+arr.sort()
+
+console.log(Math.max(...arr));
+
+
+
+
+
